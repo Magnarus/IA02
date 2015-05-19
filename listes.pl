494801463ac6b5fe1449638de12c38012e01cb85
Magnarus <delau.greg@gmail.com>
2015-05-19 13:56:26 +0200

    debut du jcj

--- listes.pl
+++ listes.pl
@@ -1,4 +1,4 @@
-bourse([[ble,7],[riz,6],[cacao,6],[cafe,6],[sucre,6],[mais,6]]).
+/* bourse([[ble,7],[riz,6],[cacao,6],[cafe,6],[sucre,6],[mais,6]]).
 marchandise([[mais,riz,ble,ble],
 			[ble,mais,sucre,riz],
 			[cafe,sucre,cacao,riz],
@@ -13,18 +13,20 @@ trader(R):-marchandise(X),length(X,L), random(1,L,R).
 
 reserves(
 [
-	[j1,[]],
-	[j2,[]]
+	[],
+	[]
 ]).
 
 
 plateau(R):-bourse(X),marchandise(Y),trader(Z),reserves(U),
-			concat(X,Y,V),ajout(Z,V,W),concat(W,U,R).
+			concat(X,Y,V),ajout(Z,V,W),concat(W,U,R). */
 
+trader_depart(R):-marchandise(X),length(X,L), random(1,L,R).
 afficher_liste([]).
 afficher_liste([X]) :- write(X).
 afficher_liste([X|L]) :- afficher_liste(X), nl, afficher_liste(L).
 
+afficher_trader:-trader(T), print(T).
 afficher_bourse:-bourse(X) ,print(X).
 afficher_marchandises:-marchandise(X), print(X).
 afficher_reserves(X):-reserves(Y), nth0(X,Y,R), print(R).
@@ -34,3 +36,27 @@ concat([],L,L).
 concat([T|L1],L2,[T|L3]):-concat(L1,L2,L3).
 
 ajout(X,L,[X|L]).
+
+init_jcj:- assertz(bourse([[ble,7],[riz,6],[cacao,6],[cafe,6],[sucre,6],[mais,6]])),
+		   assertz(reserves([j1,[]],[j2,[]])),
+           assertz(marchandise([[mais,riz,ble,ble],
+						   [ble,mais,sucre,riz],
+						   [cafe,sucre,cacao,riz],
+						   [cafe,mais,sucre,mais],
+						   [cacao,mais,ble,sucre],
+						   [riz,cafe,sucre,ble],
+						   [cafe,ble,sucre,cacao],
+						   [mais,cacao,cacao,ble],
+						   [riz,riz,cafe,cacao]])),
+		   trader_depart(T),
+		   assertz(trader(T)),
+		   assertz(plateau(R):-bourse(X),
+							   marchandise(Y),
+							   trader(Z),
+							   reserves(U),
+							   concat(X,Y,V),
+							   ajout(Z,V,W),
+							   concat(W,U,R)).
+
+jcj:- init_jcj, write("La position du trader est : "), afficher_trader, nl, 
+	  
